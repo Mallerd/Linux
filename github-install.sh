d433d616ddca8083852269d37d7dcd830c4bce1b
Frank <fa.de.haan@st.hanze.nl>
2017-06-29 13:36:23 +0200

    Tweede commit

---
+++ github-install.sh
@@ -0,0 +1,7 @@
+#Github installatie
+
+#maak directory aan genaamd Linuxrepo
+mkdir Linuxrepo
+
+#clone repository
+git clone https://github.com/Mallerd/Linux.git
---
+++ initialisatie-script.sh
@@ -0,0 +1,10 @@
+#Initialisatie script
+
+#!/bin/bash
+HST="$(hostname)"
+sed -i  "/127.0.0.1/ a\127.0.0.1 $HST" /etc/hosts
+
+sudo -s
+
+sudo apt-get -y update
+sudo apt-get -y upgrade
